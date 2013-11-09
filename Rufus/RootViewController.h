cf322d7cfb9f6238a2db35ac4419bfc5c4692b74
Karl Kittel <klkittel@karl-kittels-imac.local>
2013-11-09 16:26:39 -0500

    Initial Commit

---
+++ Rufus/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Rufus
+//
+//  Created by Karl Kittel on 11/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
